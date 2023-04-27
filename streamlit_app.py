f7b1cd92cc3536eab7115512568b687e347438ee
James Myddelton <jamesmyddelton@gmail.com>
2023-04-27 06:56:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,6 @@ streamlit.title('Test')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 🥗 🐔 🥑🍞 Hard-Boiled Free-Range Egg')
+
+
